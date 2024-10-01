0d4908df3ec340fe5a4d3004ae9c141649313019
castela0119 <78953029+castela0119@users.noreply.github.com>
2024-10-01 20:41:20 +0900

    feat : padding, margin 0 적용

--- vue-project/src/views/TheView.vue
+++ vue-project/src/views/TheView.vue
@@ -1,11 +1,16 @@
 <template>
-    <main>
-        <div class="container py-4">
-            <RouterView />
-        </div>
-    </main>
+  <main>
+    <div class="container py-4">
+      <RouterView />
+    </div>
+  </main>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.py-4 {
+  padding: 0;
+  margin: 0;
+}
+</style>
