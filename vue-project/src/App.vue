8c10ef18b47fdf0210a1b3f7b6396df3b0c2d2e8
castela0119 <78953029+castela0119@users.noreply.github.com>
2024-10-01 20:41:27 +0900

    temp

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,30 +1,28 @@
 <template>
   <TheHeader :title="headerTitle"></TheHeader>
-  
   <TheView></TheView>
 </template>
 
 <script setup>
-import { ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
-import TheView from '@/views/TheView.vue';
-import TheHeader from '@/components/TheHeader.vue';
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import TheView from '@/views/TheView.vue'
+import TheHeader from '@/components/TheHeader.vue'
 
 // headerTitle 상태를 관리
-const headerTitle = ref('모두의 문서');
+const headerTitle = ref('모두의 문서')
 
 // 현재 라우트를 가져오는 훅
-const route = useRoute();
+const route = useRoute()
 
 // 라우트가 변경될 때마다 title 업데이트
 watch(route, (newRoute) => {
   if (newRoute.name === 'Home') {
-    headerTitle.value = '모두의 문서';
+    headerTitle.value = '모두의 문서'
   } else if (newRoute.name === 'PaperType') {
-    headerTitle.value = '문서 선택';
+    headerTitle.value = '문서 선택'
   } else {
-    headerTitle.value = '기본 제목';
+    headerTitle.value = '기본 제목'
   }
-});
-
+})
 </script>
