9879034ae47b1a2814be238eb79711b50d11ade7
jinbang <castela0119@gmail.com>
2024-10-25 21:13:56 +0900

    feat : localStorage 관련코드

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -24,19 +24,13 @@ const authStore = useAuthStore()
 
 // onMounted 훅에서 로그인 상태를 복원
 onMounted(() => {
-  authStore.loadTokenFromStorage() // 앱 로드 시 localStorage에서 토큰을 다시 로드
-  const token = localStorage.getItem('userToken') // localStorage에서 토큰 가져오기
+  authStore.loadFromStorage() // 앱 로드 시 localStorage에서 토큰과 닉네임을 다시 로드
 
-  console.log('token ::: ' + token)
+  // 로그된 토큰 확인 (디버깅용)
+  console.log('token ::: ', authStore.token)
 
-  if (token) {
-    // 토큰이 있으면 로그인된 상태로 설정
-    isLoggedIn.value = true
-    // 추가로 사용자 정보를 로드하거나 토큰 검증을 서버에서 할 수도 있음
-  } else {
-    // 토큰이 없으면 로그인 해제 상태
-    isLoggedIn.value = false
-  }
+  // 토큰 상태에 따라 로그인 여부 설정
+  isLoggedIn.value = authStore.isLoggedIn
 })
 
 // 현재 경로를 확인하여 모달이 필요한 경우에만 띄우는 로직
